362c1c2eb283430f05f68e43c53c97f7948f9c87
Michael McDonnell <michael@winterstorm.ca>
2020-05-08 11:04:26 -0600

    Created gather.sh

---
+++ bin/gather.sh
@@ -0,0 +1,6 @@
+###
+### Placeholder for the gather.sh script
+###
+### Gather.sh will load the list of domain and use checkdmarc to fetch DNS records
+### it won't do anything else except gather all the dns info we want
+### 
