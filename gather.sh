93e4a7caed2837c835b1c8118d2cfc0bfebcf9c9
Michael McDonnell <michael@winterstorm.ca>
2020-05-11 21:36:47 -0600

    create gather.sh

---
+++ gather.sh
@@ -0,0 +1,35 @@
+!/bin/bash
+#############################################################################
+# Gather DMARC, DKIM, SPF, and miscelaneous DNS records for a domains
+#
+# This prototype version simply calls the executable "checkdmarc" which
+# comes from the PyPi project. It is not very fast. Eventually, we will
+# write a python application that does parallel lookups for more speed.
+#
+# Globals:
+#   none
+# Arguments:
+#   none
+# Input:
+#   /domain-data/top-1m.csv
+# Output:
+#   /domain-data/<domain>.json
+#
+# Notes:
+#   This program will read top-1m.csv line-by-line. It must contain
+#   a list of DNS domains, one per line in CSV format. The 2nd feild is
+#   the domain, the 1st field is ignored (Alexa Top 1 million format)
+#
+#   If there is not already a file named <domain>.json in the Output
+#   directory, we will run checkdmarc and output the file.
+#
+#############################################################################
+
+
+cut -d, -f2 domains.txt | while read DOMAIN
+do
+  if [[ ! -f "/data/${DOMAIN}.json" ]]
+  then
+    checkdmarc ${DOMAIN} > "/data/${DOMAIN}.json"
+  fi
+done
